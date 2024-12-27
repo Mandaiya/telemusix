3ac0c6285b3205b75427c68476d398662d9fda86
abitha000 <abithak000@yahoo.com>
2024-12-27 20:14:58 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -25,7 +25,7 @@ class Config:
     ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "7029090289"))  # Telegram ID, not Username
     AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000"))  # in seconds
     AUTO_LEAVE = os.environ.get("AUTO_LEAVING_ASSISTANT", None)  # Change it to "True"
-    COOKIES = os.getenv("COOKIES", None)  # Load the COOKIES variable
+    COOKIES = os.environ.get("COOKIES", None)  # Load the COOKIES variable
     
     # Debugging: Confirm the value of COOKIES
     print(f"COOKIES loaded: {COOKIES}")
