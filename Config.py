1ebb7767fba8b5a7d42205b2c0d26d83005d44f6
abitha000 <abithak000@yahoo.com>
2024-12-27 20:10:57 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,11 +1,16 @@
-import re
-import sys
 import os
-from os import getenv
-
 from dotenv import load_dotenv
 
-class Config(object):
+# Load environment variables from a .env file if it exists
+load_dotenv()
+
+class Config:
+    # Debugging: Print all environment variables loaded
+    print("Loaded Environment Variables:")
+    for key, value in os.environ.items():
+        if "SECRET" not in key:  # Avoid printing sensitive data
+            print(f"{key}: {value}")
+    
     API_ID = int(os.environ.get("API_ID", "28045580"))
     API_HASH = os.environ.get("API_HASH", "83001e24418ec7f54bfe95d4e390419f")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
@@ -13,12 +18,14 @@ class Config(object):
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", True)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-    SUPPORT = os.environ.get("SUPPORT", "TheSupportChat") # Your Support
-    CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel") # Your Channel
+    SUPPORT = os.environ.get("SUPPORT", "TheSupportChat")  # Your Support
+    CHANNEL = os.environ.get("CHANNEL", "TheUpdatesChannel")  # Your Channel
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
     CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
-    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "7029090289")) # telegram I'd not Username
-    AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000")) # in seconds
-    AUTO_LEAVE = os.environ.get('AUTO_LEAVING_ASSISTANT', None) # Change it to "True"
-    COOKIES = getenv("COOKIES", None)
-    # https://batbin.me
+    ASSISTANT_ID = int(os.environ.get("ASSISTANT_ID", "7029090289"))  # Telegram ID, not Username
+    AUTO_LEAVE_TIME = int(os.environ.get("AUTO_LEAVE_ASSISTANT_TIME", "54000"))  # in seconds
+    AUTO_LEAVE = os.environ.get("AUTO_LEAVING_ASSISTANT", None)  # Change it to "True"
+    COOKIES = os.getenv("COOKIES", None)  # Load the COOKIES variable
+    
+    # Debugging: Confirm the value of COOKIES
+    print(f"COOKIES loaded: {COOKIES}")
